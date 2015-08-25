1c321ed9c1d7046d0c74c3bf2bb16fd38256ca97
muukii <muukii.muukii@gmail.com>
2015-08-25 17:48:11 +0900

    Initial Commit

---
+++ AppVersionChecker/AppVersionChecker.h
@@ -0,0 +1,19 @@
+//
+//  AppVersionChecker.h
+//  AppVersionChecker
+//
+//  Created by Hiroshi Kimura on 8/25/15.
+//  Copyright © 2015 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppVersionChecker.
+FOUNDATION_EXPORT double AppVersionCheckerVersionNumber;
+
+//! Project version string for AppVersionChecker.
+FOUNDATION_EXPORT const unsigned char AppVersionCheckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppVersionChecker/PublicHeader.h>
+
+
